dee7353f7f6eacb3391cfae8b115b1c151c6f98d
isant028 <isabellasantiago2469@gmail.com>
2020-11-17 16:07:19 -0800

    added math classes from lab 4

---
+++ unit_test.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include "op_test.hpp"
+
+#include "Mult_Test.hpp"
+#include "div_test.hpp"
+#include "sub_test.hpp"
+#include "add_test.hpp" 
+#include "pow_test.hpp"
+#include "Rand_Test.hpp"
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
